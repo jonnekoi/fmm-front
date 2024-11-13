eca77e17f7b61ffd00ed0d8675934508fdf2aff8
jonnek <jonnekoivisto00@gmail.com>
2024-11-13 20:14:23 +0200

    match table no data yet

--- src/components/Matches.jsx
+++ src/components/Matches.jsx
@@ -3,7 +3,11 @@ import MatchesTable from './MatchesTable.jsx';
 const Matches = () => {
   return (
       <>
-        <MatchesTable></MatchesTable>
+        <div className="flex flex-row space-x-6 m-5 matches-mobile">
+          <MatchesTable></MatchesTable>
+          <MatchesTable></MatchesTable>
+          <MatchesTable></MatchesTable>
+        </div>
       </>
   );
 };
--- src/components/MatchesTable.jsx
+++ src/components/MatchesTable.jsx
@@ -1,27 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { format, addHours } from 'date-fns';
+
+const url = 'http://127.0.0.1:3000/v1';
+
+const fetchMatches = async (setMatches) => {
+  try {
+    const response = await fetch(url + '/matches');
+    const responseData = await response.json();
+    setMatches(responseData);
+    console.log(responseData);
+  } catch (error) {
+    console.error('Error fetching matches:', error);
+  }
+}
+
 const MatchesTable = () => {
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    fetchMatches(setMatches);
+  }, []);
+
   return (
-      <div className="w-1/5 flex flex-col">
+      <div className="w-2/6 flex flex-col matches-mobile-table">
         <h1 className="font-myFont text-6xl">Matches</h1>
-          <div className="bg-slate-900 m-auto w-full border mt-5">
-            <table className="w-full">
-              <thead>
-              <tr className="">
-                <th className="p-2 border">Match Date</th>
-                <th className="p-2 border">Home</th>
-                <th className="p-2 border">Away</th>
-                <th className="p-2 border">Score</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td className="p-2 border">Joukkue A</td>
-                <td className="p-2 border">Joukkue B</td>
-                <td className="p-2 border">0</td>
-                <td className="p-2 border">0</td>
-              </tr>
-              </tbody>
-            </table>
-          </div>
+        <div className="bg-slate-900 m-auto w-full border mt-5">
+          <table className="w-full">
+            <thead>
+            <tr>
+              <th className="p-2 border">Match Date</th>
+              <th className="p-2 border">Home</th>
+              <th className="p-2 border">Away</th>
+              <th className="p-2 border">Score</th>
+            </tr>
+            </thead>
+            <tbody>
+            {matches.map((match, index) => (
+                <tr key={index}>
+                  <td className="p-2 border">{format(new Date(match.matchday), 'yyyy-MM-dd HH:mm')}</td>
+                  <td className="p-2 border">{match.home_team}</td>
+                  <td className="p-2 border">{match.away_team}</td>
+                  <td className="p-2 border">{match.home_score} - {match.away_score}</td>
+                </tr>
+            ))}
+            </tbody>
+          </table>
+        </div>
       </div>
   );
 };

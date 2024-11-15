b1bdfdbf5e1ab4ccd0d898058b88691835ac42d2
jonnek <jonnekoivisto00@gmail.com>
2024-11-16 01:43:43 +0200

    add guess

--- src/components/SingleMatch.jsx
+++ src/components/SingleMatch.jsx
@@ -1,14 +1,68 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from 'react';
-import {differenceInMinutes, format} from 'date-fns';
+import { differenceInMinutes, format } from 'date-fns';
 
 const url = 'http://127.0.0.1:3000/v1';
+
+const handleSubmitGuess = async (event, matchid, setStatusMessage, setUserGuess) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  const matchId = matchid;
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+    },
+    body: JSON.stringify(data),
+  };
+
+  try {
+    const response = await fetch(url + `/matches/guess/${matchId}`, fetchOptions);
+    if (response.status === 304) {
+      setStatusMessage('Match already started');
+    } else if (response.ok) {
+      setStatusMessage('Guess added!');
+      setUserGuess(data.guess);
+    }
+  } catch (error) {
+    console.error('Error submitting guess:', error);
+  }
+};
+
+const fetchUserGuess = async (matchId, setUserGuess) => {
+  const userId = sessionStorage.getItem('id');
+  const fetchOptions = {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+    }
+  };
+
+  try {
+    const response = await fetch(url + `/matches/${matchId}/guess/${userId}`, fetchOptions);
+    if (response.ok) {
+      const responseData = await response.json();
+      if (responseData.length > 0) {
+        setUserGuess(responseData[0].guess);
+      }
+    }
+  } catch (error) {
+    console.error('Error fetching user guess:', error);
+  }
+};
 
 // eslint-disable-next-line react/prop-types
 const SingleMatch = ({ matchId }) => {
   const [match, setMatch] = useState(null);
+  const [statusMessage, setStatusMessage] = useState('');
+  const [userGuess, setUserGuess] = useState(null);
 
   useEffect(() => {
+    setUserGuess(null);
+    setStatusMessage('');
+
     if (matchId) {
       const fetchMatch = async () => {
         const fetchOptions = {
@@ -29,6 +83,7 @@ const SingleMatch = ({ matchId }) => {
       };
 
       fetchMatch();
+      fetchUserGuess(matchId, setUserGuess);
     }
   }, [matchId]);
 
@@ -49,6 +104,9 @@ const SingleMatch = ({ matchId }) => {
           ? `Starts in ${days}d ${hours}h ${minutes}min`
           : `Starts in ${hours}h ${minutes}min`
       : format(matchTime, 'yyyy-MM-dd HH:mm');
+
+  const matchStarted = timeDifference <= 0;
+
   return (
       <div className="w-2/6 flex flex-col m-3 match-detail-mobile">
         <h1 className="font-myFont text-6xl">Match Details</h1>
@@ -57,11 +115,21 @@ const SingleMatch = ({ matchId }) => {
           <p className="p-2 font-myFont text-6xl"><strong>Home: </strong> {match.home_team}</p>
           <p className="p-2 font-myFont text-6xl"><strong>Away: </strong> {match.away_team}</p>
           <p className="p-2 font-myFont text-6xl border-b"><strong>Score: </strong> {homeScore} - {awayScore}</p>
-          <form className="flex flex-col m-auto w-3/4 profile-form">
-          <label className="p-2 font-myFont text-6xl">Enter Score (e.g. 1-1):</label>
-          <input className="m-2 bg-white rounded text-black p-1 text-center " type="text" id="score" name="score" pattern="^[0-99]-[0-99]$" required/>
-          <button className="font-myFont mt-5 text-6xl hover:underline" type="submit">Submit</button>
-          </form>
+          {userGuess && <p className="p-2 font-myFont text-6xl border-b"><strong>Your Guess: </strong> {userGuess}</p>}
+          {!matchStarted && (
+              <form onSubmit={(e) => handleSubmitGuess(e, matchId, setStatusMessage, setUserGuess)}
+                    className="flex flex-col m-auto w-3/4 profile-form">
+                <label className="p-2 font-myFont text-6xl">Enter Score (e.g. 1-1):</label>
+                <input className="m-2 bg-white rounded text-black p-1 text-center "
+                       type="text" id="score" name="guess" pattern="^[0-9]{1,2}-[0-9]{1,2}$"
+                       defaultValue={userGuess || ''}
+                       required/>
+                <button className="font-myFont mt-5 text-6xl hover:underline"
+                        type="submit">Submit
+                </button>
+              </form>
+          )}
+          {statusMessage && <p className="p-2 font-myFont text-6xl text-green-500">{statusMessage}</p>}
         </div>
       </div>
   );
